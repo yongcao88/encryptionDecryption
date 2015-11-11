7126906dadda910bd123ec2eb568654c5aa1c577
Yong <yongcao88@gmail.com>
2015-11-11 13:11:49 +1100

    Update v0.1

--- src/substitutionEncryption.java
+++ src/substitutionEncryption.java
@@ -1,16 +1,22 @@
 import java.util.Scanner;
 
 /**
+ * @author Yong Cao
+ * @version 0.1
  * Created by ycao on 11/11/2015.
  */
 public class substitutionEncryption {
     public static void main (String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a word/sentence to encrypt/decrypt:");
-        String str = scanner.nextLine();
-        System.out.println("Select 1. to Encrypt, select 2 to Decrypt:");
+        System.out.println("Welcome to Substitution Encryption/Decription");
+        System.out.println("V0.1 11/11/2015");
+        System.out.println("Please select from following options:");
+        System.out.println("1 - Encrypt");
+        System.out.println("2 - Decrypt");
         int selection = 0;
         selection = scanner.nextInt();
+        System.out.println("Please enter a word or sentence to encrypt/decrypt:");
+        String str = scanner.nextLine();
 
         //algorithm
         String plainText = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ";
@@ -27,26 +33,26 @@ public class substitutionEncryption {
         }
     }
 
-    public static String encrypt(String strOriginal, String from, String to){
+    public static String encrypt(String strOriginal, String plainText, String cipherText){
         StringBuilder output = new StringBuilder();
         char[] letters = strOriginal.toCharArray();
         for (int i = 0; i < letters.length; i++){
-            for (int j = 0; j < from.length(); j++){
-                if (letters[i] == from.charAt(j)){
-                    output.append(to.charAt(j));
+            for (int j = 0; j < plainText.length(); j++){
+                if (letters[i] == plainText.charAt(j)){
+                    output.append(cipherText.charAt(j));
                 }
             }
         }
         return output.toString();
     }
 
-    public static String decrypt(String strCipher, String from, String to){
+    public static String decrypt(String strCipher, String cipherText, String plainText){
         StringBuilder output = new StringBuilder();
         char[] letters = strCipher.toCharArray();
         for (int i = 0; i < letters.length; i++){
-            for (int j = 0; j < from.length(); j++){
-                if (letters[i] == from.charAt(j)){
-                    output.append(to.charAt(j));
+            for (int j = 0; j < cipherText.length(); j++){
+                if (letters[i] == cipherText.charAt(j)){
+                    output.append(plainText.charAt(j));
                 }
             }
         }
