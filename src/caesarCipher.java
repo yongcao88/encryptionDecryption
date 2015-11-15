5b909929b18c82563794bea36a0f64494f67bbb1
ycao <yongcao88@gmail.com>
2015-11-16 09:15:13 +1100

    Update v0.2.1

--- src/caesarCipher.java
+++ src/caesarCipher.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
 
 /**
+ * This example explains when key = 3, Caesar substitution encryption
  * @author Yong Cao
- * @version 0.2
+ * @version 0.2.1
  * Created by ycao on 11/11/2015.
  */
-public class substitutionEncryption {
+public class caesarCipher {
     public static void main (String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to Substitution Encryption/Decryption");
-        System.out.println("V0.2 11/11/2015");
+        System.out.println("This is an example of Caesar cipher when key = 3");
+        System.out.println("V0.2.1 16/11/2015");
         System.out.println("Please enter a word or sentence to encrypt/decrypt:");
         String str = scanner.nextLine();
         System.out.println("Please select from following options:");
@@ -17,10 +18,9 @@ public class substitutionEncryption {
         System.out.println("2 - Decrypt");
         int selection = scanner.nextInt();
 
-
         //algorithm
         String plainText = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ";
-        String cipherText = "XYZABCDEFGHIJKLMNOPQRSTUVWxyzabcedefhijklmnopqrstuvw8901234567 ";
+        String cipherText = "xyzabcedefhijklmnopqrstuvwXYZABCDEFGHIJKLMNOPQRSTUVW8901234567 ";
 
         if (selection == 1){
             System.out.println(encrypt(str, plainText, cipherText));
@@ -33,6 +33,13 @@ public class substitutionEncryption {
         }
     }
 
+    /**
+     * Encrypt text by passing the plain text
+     * @param strOriginal
+     * @param plainText
+     * @param cipherText
+     * @return encrypted text
+     */
     public static String encrypt(String strOriginal, String plainText, String cipherText){
         StringBuilder output = new StringBuilder();
         char[] letters = strOriginal.toCharArray();
@@ -46,6 +53,13 @@ public class substitutionEncryption {
         return output.toString();
     }
 
+    /**
+     * Decrypt text by passing the cipher text
+     * @param strCipher
+     * @param cipherText
+     * @param plainText
+     * @return decrypted text
+     */
     public static String decrypt(String strCipher, String cipherText, String plainText){
         StringBuilder output = new StringBuilder();
         char[] letters = strCipher.toCharArray();
